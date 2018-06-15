0401904eea0bcf78d3b30c39c239e9163616ac20
John Kusching <kimshrum@Kims-Air.home>
2018-06-15 18:11:18 -0500

    Finished basic CSS layout

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
+// textBox
+// sleepButton
+// retreatButton
+// healButton
+// attackButton
+// downButton
+// rightButton
+// leftButton
+// upButton
+// playerPotions
+// playerHealth
+// playerName
+// resetButton
+// howToPlayButton
+// aboutButton
+// mainSection (class)
+
 $( () => {
-  console.log("YOLO DUDE!");
+  // generateDivs();
 });
