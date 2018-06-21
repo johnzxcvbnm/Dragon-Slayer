13935308f17f6b883fbcdb109747583a7d3d0b9b
John Kusching <kimshrum@Kims-Air.home>
2018-06-21 12:10:19 -0500

    Added button functionality to the misc buttons.  Added an About and How to Play modals to the game.

--- Games/DragonSlayer/js/miscButton.js
+++ Games/DragonSlayer/js/miscButton.js
@@ -1,7 +1,25 @@
 //Enables all the functionality of the misc buttons
 //Misc buttons never get turned off
 const enableMiscButtons = () => {
-  $("#aboutButton").on("click", () => { console.log("About was clicked"); });
-  $("#howToPlayButton").on("click", () => { console.log("How to play was clicked"); });
+  $("#aboutButton").on("click", aboutModal);
+  $("#howToPlayButton").on("click", playModal);
   $("#resetButton").on("click", resetGame );
+  $("#closeButton").on("click", closeModal);
+  $("#closePlayButton").on("click", closeModal);
+}
+
+const closeModal = () => {
+  $("#modal-aboutTextbox").hide(400);
+  $("#modal-playTextbox").hide(400);
+  $("#modal").hide(400);
+}
+
+const aboutModal = () => {
+  $("#modal").show(400);
+  $("#modal-aboutTextbox").show(400);
+}
+
+const playModal = () => {
+  $("#modal").show(400);
+  $("#modal-playTextbox").show(400);
 }
